22dc0df73d74d41f01c9675324cb5f24ed14718a
LuisPineda <luis.pineda.revelo@udla.edu.ec>
2026-03-25 00:32:57 -0500

    feat: Redireccionamiento al dashboard como paciente

--- src/app/features/paciente/paciente-module.ts
+++ src/app/features/paciente/paciente-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PacienteRoutingModule } from './paciente-routing-module';
+import { SharedModule } from '../../shared/shared-module';
+import { Layout } from './layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, PacienteRoutingModule],
+  declarations: [
+    Layout,
+    Dashboard,
+  ],
+  imports: [
+    CommonModule,
+    PacienteRoutingModule,
+    SharedModule,
+  ],
 })
 export class PacienteModule {}
